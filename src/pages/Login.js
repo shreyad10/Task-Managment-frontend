5c8c5e4649c2b27ddc44d33e9bc9b53e00cdb2cb
ShreyaDWappgo <shreya.dubey@wappgo.in>
2024-07-10 16:19:28 +0530

    update new

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,17 +3,23 @@ import { login } from '../api/api';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate, Link } from 'react-router-dom';
+import ReCAPTCHA from 'react-google-recaptcha';
 import './Login.css'; // Import custom CSS for additional styling
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [recaptchaToken, setRecaptchaToken] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!recaptchaToken) {
+      toast.error('Please complete the reCAPTCHA');
+      return;
+    }
     try {
-      const response = await login({ email, password });
+      const response = await login({ email, password, recaptchaToken });
       toast.success(response.data.message);
       const authToken = response.data.token;
       localStorage.setItem('authToken', authToken);
@@ -28,6 +34,10 @@ const Login = () => {
         toast.error('Failed to login. Please try again.');
       }
     }
+  };
+
+  const handleRecaptchaChange = (token) => {
+    setRecaptchaToken(token);
   };
 
   return (
@@ -56,6 +66,12 @@ const Login = () => {
                   placeholder="Enter password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="form-group d-flex justify-content-center">
+                <ReCAPTCHA
+                  sitekey="6Lf8tAwqAAAAACtQ-HDWhjlE4Jd-Mj1l1DBXfq9x" // replace with your site key
+                  onChange={handleRecaptchaChange}
                 />
               </div>
               <div className="d-grid gap-2">
